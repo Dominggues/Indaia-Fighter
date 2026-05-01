7e79dd8fb765fec09bd41f058fce378a24211de0
dominggues <guilherme18domingues@gmail.com>
2026-04-30 22:01:00 -0300

    Adicionando barra de vida, adicionando lógica da vida, adicionando fonte de texto e corrigindo alguns bugs de interface da barra de vida

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI; // ESSENCIAL: Permite que o código converse com o Slider e imagens da UI
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,17 +13,33 @@ public class PlayerController : MonoBehaviour
     public int addJumps;
     public bool isGrounded;
     public float jumpForce;
-    public int life;
+
+    [Header("Sistema de Vida")]
+    public int maxLife = 100;
+    public int currentLife;
+    public Slider healthSlider; // É AQUI QUE VOCÊ VAI ARRASTAR O SEU SLIDER "VidaP1"
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        // Enche a vida quando a luta começa
+        currentLife = maxLife;
+        if (healthSlider != null)
+        {
+            healthSlider.maxValue = maxLife;
+            healthSlider.value = currentLife;
+        }
     }
 
     void Update()
     {
         moveX = Input.GetAxisRaw("Horizontal");
+
+        // Os ataques vieram para o Update para não falharem o clique!
+        Attack();
+        Attack2();
 
         if (isGrounded == true)
         {
@@ -40,14 +57,18 @@ public class PlayerController : MonoBehaviour
                 Jump();
             }
         }
+
+        // BOTAO DE TESTE: Aperte 'T' no teclado para ver se a barra está perdendo vida
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
     }
 
     void FixedUpdate()
     {
         // Aqui dentro deixamos apenas o que é contínuo (andar de um lado para o outro)
         Move();
-        Attack();
-        Attack2();
     }
 
     void Move()
@@ -59,7 +80,6 @@ public class PlayerController : MonoBehaviour
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
             anim.SetBool("IsRun" , true);
         }
-
 
         if(moveX < 0) // SE O PLAYER ESTIVER OLHANDO PARA O LADO ESQUERDO
         {
@@ -93,6 +113,36 @@ public class PlayerController : MonoBehaviour
         {
             anim.Play("Attack1", -1);
         }
+    }
+
+    // --- NOVA LÓGICA DE SOFRER DANO ---
+    public void TakeDamage(int damageAmount)
+    {
+        currentLife -= damageAmount;
+
+        // Impede que a vida fique negativa (ex: -10)
+        if (currentLife < 0)
+        {
+            currentLife = 0;
+        }
+
+        // Atualiza a barrinha visual lá no topo da tela
+        if (healthSlider != null)
+        {
+            healthSlider.value = currentLife;
+        }
+
+        // Verifica se morreu
+        if (currentLife == 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("K.O.! Jogador foi derrotado!");
+        // Futuramente colocaremos a animação de morte aqui: anim.Play("Die");
     }
 
     void OnCollisionEnter2D(Collision2D collision)
